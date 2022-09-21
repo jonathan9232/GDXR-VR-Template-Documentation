1c2469cd363cf8e21c1c192b1bbc1753bda2b980
Jonathan Bardwell <33333058+jonathan9232@users.noreply.github.com>
2022-09-21 13:59:12 +0100

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,12 +2,12 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'GDXR VR Template'
+copyright = '2022, Jonathan Bardwell'
+author = 'Jonathan Bardwell'
 
-release = '0.1'
-version = '0.1.0'
+release = '0.3.0'
+version = '0.3.0'
 
 # -- General configuration
 
